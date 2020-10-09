1aacfff4c5fc48fef8ce7969696a2d75dea1cdfb
ikekou0015 <ikekou0015@gmail.com>
2020-10-09 23:02:34 +0900

    プルダウンの実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,8 +1,4 @@
 class RoomUser < ApplicationRecord
-  def change
-    create_table :room_users do |t|
-      t.refrences :room, foreign_key: true
-      t.refrences :user, foreign_key: true
-      t.timestamps
-  foreign_key: true 
+    belongs_to :room
+    belongs_to :user
 end
